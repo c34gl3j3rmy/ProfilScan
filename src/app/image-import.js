8a3efdede3bfe3ba024fb0eff16ce28f767283f8
c34gl3j3rmy <legacjeremy@gmail.com>
2026-07-08 14:38:03 +0200

    Rasterise les SVG importes en image

--- src/app/image-import.js
+++ src/app/image-import.js
@@ -1,4 +1,7 @@
+import { isSvgFile, renderSvgFileToBitmap } from './svg-rasterizer.js';
+
 export async function loadImageFile(file) {
   if (!file) throw new Error('Aucun fichier image selectionne.');
+  if (isSvgFile(file)) return renderSvgFileToBitmap(file);
   return createImageBitmap(file);
 }
