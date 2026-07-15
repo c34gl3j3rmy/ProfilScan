f79b845c075e6770ed250e24a3e52b6f7348720e
c34gl3j3rmy <legacjeremy@gmail.com>
2026-07-15 10:17:57 +0200

    Ajoute la signature structurelle explicable

---
+++ src/shape-engine/structural-signature.js
@@ -0,0 +1,11 @@
+import { registerAlgorithm } from '../observability/algorithm-registry.js';
+
+const DEFAULT_GRID_SIZE = 96;
+const DEFAULT_PROJECTION_BINS = 12;
+
+export function buildStructuralSignature(contours, options = {}) {
+  const gridSize = positiveInteger(options.gridSize, DEFAULT_GRID_SIZE);
+  const projectionBins = positiveInteger(options.projectionBins, DEFAULT_PROJECTION_BINS);
+  const normalizedContours = normalizeContours(contours);
+  const mask = rasterizeContours(normalizedContours, gridSize);
+  const
